131de7cc9d4c8445c228c02ed63834990ce4651c
Stephen Hodgson <hodgson.designs@gmail.com>
2020-05-22 18:27:59 -0400

    removed debug code and cleaned up platform class a bit

--- XRTK.Nreal/Packages/com.xrtk.nreal/Runtime/NrealPlatform.cs
+++ XRTK.Nreal/Packages/com.xrtk.nreal/Runtime/NrealPlatform.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
+using System.Runtime.InteropServices;
 using NRKernal;
 using UnityEngine;
 using XRTK.Interfaces;
@@ -11,26 +12,18 @@ namespace XRTK.Definitions.Platforms
     /// <summary>
     /// Used by the XRTK to signal that the feature is available on the Nreal platform.
     /// </summary>
-    [System.Runtime.InteropServices.Guid("1C4598F7-A765-4934-A4B2-3E3F451F87A7")]
+    [Guid("1C4598F7-A765-4934-A4B2-3E3F451F87A7")]
     public class NrealPlatform : BasePlatform
     {
-        private static readonly System.Version NoVersion = new System.Version();
-        private static System.Version NRSDKVersion
-        {
-            get
-            {
-                var versionString = NRVersionInfo.GetVersion();
-                Debug.Log($"Found NRSDK Version: {versionString}");
-                return new Version();
-            }
-        }
+        private static readonly Version NoVersion = new Version();
+        private static Version NRSDKVersion => new Version(NRVersionInfo.GetVersion());
 
         /// <inheritdoc />
         public override bool IsAvailable
         {
             get
             {
-                return !UnityEngine.Application.isEditor && NRSDKVersion > NoVersion;
+                return !Application.isEditor && NRSDKVersion > NoVersion;
             }
         }
 
