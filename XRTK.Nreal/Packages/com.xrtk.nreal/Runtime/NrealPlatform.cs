bac4c8485e418591e31e3c8022d4f7c7f5ff5030
Stephen Hodgson <hodgson.designs@gmail.com>
2020-05-22 18:25:12 -0400

    Added platform version check
    Will break build. Need to figure out a good way to automatically get and reference sdk

--- XRTK.Nreal/Packages/com.xrtk.nreal/Runtime/NrealPlatform.cs
+++ XRTK.Nreal/Packages/com.xrtk.nreal/Runtime/NrealPlatform.cs
@@ -1,7 +1,9 @@
 // Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using XRTK.Definitions.Platforms;
+using System;
+using NRKernal;
+using UnityEngine;
 using XRTK.Interfaces;
 
 namespace XRTK.Definitions.Platforms
@@ -12,12 +14,23 @@ namespace XRTK.Definitions.Platforms
     [System.Runtime.InteropServices.Guid("1C4598F7-A765-4934-A4B2-3E3F451F87A7")]
     public class NrealPlatform : BasePlatform
     {
+        private static readonly System.Version NoVersion = new System.Version();
+        private static System.Version NRSDKVersion
+        {
+            get
+            {
+                var versionString = NRVersionInfo.GetVersion();
+                Debug.Log($"Found NRSDK Version: {versionString}");
+                return new Version();
+            }
+        }
+
         /// <inheritdoc />
         public override bool IsAvailable
         {
             get
             {
-                return !UnityEngine.Application.isEditor; // TODO Check native api
+                return !UnityEngine.Application.isEditor && NRSDKVersion > NoVersion;
             }
         }
 
@@ -27,7 +40,7 @@ namespace XRTK.Definitions.Platforms
             get
             {
 #if UNITY_EDITOR
-                return UnityEditor.EditorUserBuildSettings.activeBuildTarget == UnityEditor.BuildTarget.Android; // TODO Check native api
+                return UnityEditor.EditorUserBuildSettings.activeBuildTarget == UnityEditor.BuildTarget.Android && NRSDKVersion > NoVersion;
 #else
                 return false;
 #endif
